516de6273ffc52016b80e5c21253d1c0a5c87d47
David Valdiviezo <davidjujuy2017@gmail.com>
2025-07-22 22:57:46 -0300

    Se crea el composable RemoteIcon y se corrige el inicio del Navigation

---
+++ CityApp/app/src/main/java/com/david/cityapp/presentation/ui/components/RemoteIcon.kt
@@ -0,0 +1,29 @@
+package com.david.cityapp.presentation.ui.components
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+
+@Composable
+fun RemoteIcon(
+    imageUrl: String,
+    contentDescription: String?,
+    modifier: Modifier = Modifier
+) {
+    AsyncImage(
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(imageUrl)
+            .crossfade(true)
+            .build(),
+        contentDescription = contentDescription,
+        modifier = modifier
+            .size(150.dp)
+            .clip(CircleShape)
+    )
+}
--- CityApp/app/src/main/java/com/david/cityapp/presentation/ui/main/MainActivity.kt
+++ CityApp/app/src/main/java/com/david/cityapp/presentation/ui/main/MainActivity.kt
@@ -4,16 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
 import com.david.cityapp.presentation.common.theme.CityAppTheme
-import com.david.cityapp.presentation.ui.screens.citylist.CityListScreen
+import com.david.cityapp.presentation.navigation.AppNavigation
 import com.david.cityapp.presentation.ui.screens.citylist.CityListViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -23,29 +23,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CityAppTheme {
-                CityListScreen(
-                    modifier = Modifier,
-                    onCityClick = {},
-                    onClickToDetails = {},
-                    viewModel = viewModel<CityListViewModel>()
-                )
+                Surface(
+                    modifier = Modifier.Companion.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavigation(
+                        navController = rememberNavController(),
+                        startDestination = "cities",
+                        viewModel = viewModel<CityListViewModel>()
+                    )
+                }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CityAppTheme {
-        Greeting("Android")
     }
 }
