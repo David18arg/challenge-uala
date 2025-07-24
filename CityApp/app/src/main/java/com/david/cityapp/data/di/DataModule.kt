6b732d3ffa8b71fac3904b14eba3c13f27dde53b
David Valdiviezo <davidjujuy2017@gmail.com>
2025-07-24 09:12:11 -0300

    Injecto weatherRepositorio la modulo

--- CityApp/app/src/main/java/com/david/cityapp/data/di/DataModule.kt
+++ CityApp/app/src/main/java/com/david/cityapp/data/di/DataModule.kt
@@ -2,8 +2,11 @@ package com.david.cityapp.data.di
 
 import com.david.cityapp.data.local.dao.CityDao
 import com.david.cityapp.data.remote.CityApi
+import com.david.cityapp.data.remote.WeatherApi
 import com.david.cityapp.data.repository.CityRepositoryImpl
+import com.david.cityapp.data.repository.WeatherRepositoryImpl
 import com.david.cityapp.domain.repository.CityRepository
+import com.david.cityapp.domain.repository.WeatherRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -23,6 +26,16 @@ object DataModule {
         return CityRepositoryImpl(
             dao = dao,
             api = api
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideWeatherRepository(
+        weatherApi: WeatherApi,
+    ): WeatherRepository {
+        return WeatherRepositoryImpl(
+            weatherApi = weatherApi
         )
     }
 }
